d55a6af13d52081ff19324679a24abdad349cd6c
JuanMHuerta <jmhuertany@gmail.com>
2019-01-09 19:26:15 -0500

    parse xml

--- v00.py
+++ v00.py
@@ -1,6 +1,15 @@
 
-
 import sys
+from xmljson import badgerfish as bf
+from xml.etree.ElementTree import fromstring
+from json import dumps
+
+my_data = eval(dumps(bf.data(fromstring(open('2007129462.gpx','rt').read()))))
+a='{http://www.topografix.com/GPX/1/1}'
 
 
-print "GPX analytics"
+for p in my_data[a+"gpx"][a+"trk"][a+"trkseg"][a+"trkpt"]:
+    print (p[a+"time"]['$'],  float(p['@lon']), float(p[a+"ele"]['$']),  float(p['@lat']))
+
+
+    
