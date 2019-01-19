82113a6f7ac2e2ec5cc6fbc4720561ef412daf16
JuanMHuerta <jmhuertany@gmail.com>
2019-01-19 18:31:41 -0500

    Created GPX_Analytics class and moved all analytic steps into class

--- v00.py
+++ v00.py
@@ -33,42 +33,6 @@ def do_Analysis():
     my_segment.compute_power_spectrum()
     my_segment.make_plots()
 
-# Plotting steps.
-# Points are NOT evenly spaced, but let's ignore that fact for now
-    just_X=np.array(self.just_X)
-
-#  Log-Power Spectrum
-    ps = np.log(np.abs(np.fft.fft(just_X_pace))**2)
-#print ps
-    plt.figure(1)
-
-# Plot results
-    plt.subplot(5,1,1)
-    plt.title("Raw velocity points")
-    plt.plot([x[0] for x in Xd], [x[1] for x in Xd], 'bo')
-
-    plt.subplot(5,1,2)
-    plt.title("Low-pass filtered velocity Curve")
-    plt.plot([x[0] for x in Xf], [x[1] for x in Xf])
-    x=[x[1] for x in Xf]
-    x_p=[1.0/x[1] for x in Xf]
-
-    plt.subplot(5,1,3)
-    plt.title("Pace  Curve ")
-    pc =[1.0/x[1] for x in Xf]
-    plt.plot([x[0] for x in Xf],pc) 
-    plt.ylim(max(pc),min(pc))
-
-    plt.subplot(5,1,4)
-    plt.title("Log Power Spectrum of the Whole Velocity Run")
-    plt.plot(ps)
-
-    plt.subplot(5,1,5)
-    NFFT = 64  # the length of the windowing segments
-    plt.title("Changing Log Power Spectrum (Spectrogram)")
-    Pxx, freqs, bins, im = plt.specgram(x_p, NFFT=NFFT, Fs=1.0, noverlap=0 )
-    plt.show()
-
 
 
 if __name__ == "__main__":
