f4080fc29a7ad47f98cfb8699c2da047827f3f38
JuanMHuerta <jmhuertany@gmail.com>
2019-01-28 21:46:50 -0500

    Use of the sequence join function

---
+++ src/v01.py
@@ -0,0 +1,72 @@
+
+#!/usr/bin/env python
+
+"""
+
+    v00.py: Merges 2 files
+
+
+"""
+
+__author__      = "Juan M. Huerta"
+__copyright__   = "Copyright 2019, Juan M. Huerta"
+
+import sys
+import gpx_analytics
+import gpx_analytics.segment_analytics
+import gpx_analytics.segment_analytics.segment_analytics_object
+import gpx_analytics.algorithms
+import gpx_analytics.algorithms.distances
+
+#import  gpx_analytics.segment_analytics.segment_analytics_object from gpx_analytics.segment_analytics
+#from gpx_analytics.segment_analytics import  segment_analytics_object
+
+
+def do_Analysis():
+
+    file_name=None
+    my_segment_1 = gpx_analytics.segment_analytics.segment_analytics_object.segment_analytics_object()
+    my_segment_2 = gpx_analytics.segment_analytics.segment_analytics_object.segment_analytics_object()
+
+    ##  
+    if len(sys.argv) != 3:
+        print("ERROR: argument(s) missing.\n USE:  v00.py  <FILE_1>  <FILE_2>")
+        return 
+
+
+    if len(sys.argv)==3  and 'gpx' in sys.argv[1]:
+        file_name = sys.argv[1]
+        my_segment_1.load_gpx(file_name)
+    elif len(sys.argv)==3  and 'csv' in sys.argv[1]:
+        file_name = sys.argv[1]
+        my_segment_1.load_csv(file_name)
+    else:
+        print("No valid file specified")
+        return
+
+
+
+
+    if len(sys.argv)==3  and 'gpx' in sys.argv[2]:
+        file_name = sys.argv[2]
+        my_segment_2.load_gpx(file_name)
+    elif len(sys.argv)==3  and 'csv' in sys.argv[2]:
+        file_name = sys.argv[2]
+        my_segment_2.load_csv(file_name)
+    else:
+        print("No valid file specified")
+        return
+
+
+    
+    gpx_analytics.algorithms.distances.join(my_segment_1,my_segment_2)
+
+
+
+
+    return 
+
+
+if __name__ == "__main__":
+    do_Analysis()
+
